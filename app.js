bdd28701413d105aff9d4bab697430ed9e433a48
anirudha <anirudhamahale@gmail.com>
2019-05-01 15:01:25 +0530

    function types.

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 "use strict";
-var my_name = 'Anirudha';
-console.log(my_name);
-my_name = 'Ani';
-console.log(my_name);
-// This here will throw an error while you convert the the file to js as you can't change the variable type once it is declared or initialised in typescript.
-my_name = 2018;
+function sayHello() {
+    console.log('Hello');
+}
+function multiply(value1, value2) {
+    return value1 * value2;
+}
+console.log(multiply(2, 4));
+var any_variable; // Currently this variable can hold any functions as the variable is declared as any type.
+any_variable = sayHello;
+any_variable();
+any_variable = multiply;
+console.log(any_variable(1, 1));
+// To explicity tell the variable to hold the function of particular type we have to declared the variable of the respected type.
+var multiply_variable;
+multiply_variable = multiply;
+console.log(multiply_variable(20, 20));
