6359cbc727b380d42619bce08662173bdf7aaa7b
anirudha <anirudhamahale@gmail.com>
2019-05-01 11:24:42 +0530

    number_booleans

--- app.ts
+++ app.ts
@@ -1,10 +1,8 @@
+// string
 let my_name = 'Anirudha';
-console.log(my_name);
 
-my_name = 'Ani';
-console.log(my_name);
+// number
+let my_age = 25;
 
-// This here will throw an error while you convert the the file to js as you can't change the variable type once it is declared or initialised in typescript.
-my_name = 2018;
-
-
+// boolean
+let hasHobbies = true;
