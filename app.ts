8fad365914b4e0615e47539e18e329e2f7a941b3
anirudha <anirudhamahale@gmail.com>
2019-05-01 14:25:46 +0530

    Data types.

--- app.ts
+++ app.ts
@@ -6,3 +6,16 @@ let my_age = 25;
 
 // boolean
 let hasHobbies = true;
+
+const hobbies = ['Reading', 'Travelling', 'Eating'];
+
+let allowed_alphabets = ['a', 'b', 'c', 'd'];
+
+let person: [string, number] = ['Anirudha', 25];
+
+enum Colors {
+    red,
+    green,
+    blue
+}
+let color = Colors.green;
