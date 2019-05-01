fa26845a4f112e0f081f4a07efc0d70b6b8fa705
anirudha <anirudhamahale@gmail.com>
2019-05-01 11:21:19 +0530

    type_basics

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 "use strict";
-console.log('It works');
+var my_name = 'Anirudha';
+console.log(my_name);
+my_name = 'Ani';
+console.log(my_name);
+// This here will throw an error while you convert the the file to js as you can't change the variable type once it is declared or initialised in typescript.
+my_name = 2018;
--- app.ts
+++ app.ts
@@ -1,1 +1,10 @@
-console.log('It works');
+let my_name = 'Anirudha';
+console.log(my_name);
+
+my_name = 'Ani';
+console.log(my_name);
+
+// This here will throw an error while you convert the the file to js as you can't change the variable type once it is declared or initialised in typescript.
+my_name = 2018;
+
+
