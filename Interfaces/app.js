e429235269fa5f799cf2b1375644d1717a8455f1
anirudha <anirudhamahale@gmail.com>
2019-05-05 20:04:36 +0530

    Interface inheritance

--- Interfaces/app.js
+++ Interfaces/app.js
@@ -34,3 +34,15 @@ doubleFunction = function (val1, val2) {
     return val1 + val2;
 };
 console.log(doubleFunction(20, 40));
+var Person2 = /** @class */ (function () {
+    function Person2() {
+        this.age = 100;
+        this.firstName = 'Thanos';
+    }
+    Person2.prototype.greet = function (lastName) {
+        console.log("Hi my self " + this.firstName + " " + lastName);
+    };
+    return Person2;
+}());
+var oldPerson = new Person2();
+oldPerson.greet('Athanasia');
--- Interfaces/app.ts
+++ Interfaces/app.ts
@@ -1,13 +1,13 @@
 interface NamedPerson {
     firstName: String
-    age?: number // Optional property.
+    age?: number
     [propName: string]: any // We don't know the name of the key. The property name if flexible here.
 
     greet(lastName: String): void
 }
 
 function greet(person: NamedPerson) {
-    console.log('Hello, '+person.firstName)
+    console.log('Hello, ' + person.firstName)
 }
 
 function changeName(person: NamedPerson) {
@@ -31,7 +31,7 @@ person.greet('Mahale')
 
 // greet({firstName: 'Anirudha', age: 20, hobbies: ['cooking', 'sports']})
 
-class Person implements NamedPerson {  
+class Person implements NamedPerson {
     firstName = 'Anirudha'
 
     greet(lastName: string): void {
@@ -49,8 +49,25 @@ interface DoubleValueFunc {
 }
 
 let doubleFunction: DoubleValueFunc
-doubleFunction = function(val1: number, val2: number) {
+doubleFunction = function (val1: number, val2: number) {
     return val1 + val2
 }
 
 console.log(doubleFunction(20, 40))
+
+// Interface Inheritance
+interface AgedPerson extends NamedPerson {
+    age: number
+}
+
+class Person2 implements AgedPerson {
+    age = 100
+    firstName = 'Thanos'
+
+    greet(lastName: String): void {
+        console.log(`Hi my self ${this.firstName} ${lastName}`)
+    }
+}
+
+const oldPerson = new Person2()
+oldPerson.greet('Athanasia')
