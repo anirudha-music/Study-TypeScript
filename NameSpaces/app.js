e8670727edafe277f51c128d8b01ba15f22e8106
Anirudha <anirudhamusic.am@gmail.com>
2019-05-05 18:46:48 +0530

    Done with namespaces

--- NameSpaces/app.js
+++ NameSpaces/app.js
@@ -1,26 +1,12 @@
-var MyMaths;
-(function (MyMaths) {
-    var PI = 3.14;
-    function calculateCircumference(diameter) {
-        return diameter * PI;
-    }
-    MyMaths.calculateCircumference = calculateCircumference;
-})(MyMaths || (MyMaths = {}));
-var MyMaths;
-(function (MyMaths) {
-    var PI = 3.14;
-    function calculateRectangle(width, length) {
-        return width * length;
-    }
-    MyMaths.calculateRectangle = calculateRectangle;
-})(MyMaths || (MyMaths = {}));
 console.log('Example of namespaces');
+/// <reference path="./circleMath.ts" />
+/// <reference path="./rectangleMath.ts" />
 console.log(MyMaths.calculateCircumference(2));
 console.log(MyMaths.calculateRectangle(4, 6));
 /*
 
     Run the below command to merge the files.
     tsc --outFile app.js circleMath.ts rectangleMath.ts app.ts
-    The `outFile` bundles the specified files together into the repsected file.
+    The `outFile` bundles the specified files into the repsected file.
 
 */ 
--- NameSpaces/app.ts
+++ NameSpaces/app.ts
@@ -1,5 +1,8 @@
 console.log('Example of namespaces')
 
+/// <reference path="circleMath.ts" />
+/// <reference path="rectangleMath.ts" />
+
 console.log(MyMaths.calculateCircumference(2))
 console.log(MyMaths.calculateRectangle(4, 6))
 
@@ -7,6 +10,6 @@ console.log(MyMaths.calculateRectangle(4, 6))
 
     Run the below command to merge the files.
     tsc --outFile app.js circleMath.ts rectangleMath.ts app.ts
-    The `outFile` bundles the specified files together into the repsected file.
+    The `outFile` bundles the specified files into the repsected file.
 
 */
